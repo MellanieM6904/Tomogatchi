d526fe0591fc2c3cdf7d2a0ab7fc203b7f3b6365
MellanieM6904 <rsmellaniem@gmail.com>
2023-01-24 17:33:34 -0800

    init

---
+++ evolveFn.py
@@ -0,0 +1,12 @@
+exp = 0
+
+threshold1 = 10
+threshold2 = 50
+
+def evolve(exp):
+    if exp == threshold1:
+        print("") # placeholder command
+        # insert respective ASCII art fn here - hatchling
+    if exp == threshold2:
+        print("") # placeholder command
+        # insert respective ASCII art fn here - adult
