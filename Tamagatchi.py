9cc9c9b6828c3e3546cd433f9f8e6d5ebe8e0639
Fiberoptic585 <sgtspring2020@gmail.com>
2023-02-23 14:37:02 -0800

    Update Tamagatchi.py
    nvm didn't need half of that. pls ignore the last commit.

--- Tamagatchi.py
+++ Tamagatchi.py
@@ -1,27 +1,12 @@
 # class used to define a tamagatchi and it's stats
 class Pet:
     # construct class with these variables
-    def __init__(self, name, age, exp, food, filth, fun):
+    def __init__(self, name):
         self.name = name # name, used for filename of stored Pet
-        self.age = age # how old in minutes the tamagatchi is since creation
-        self.exp = exp # arbitrary points for something
         
-        self.food = food # stat for non-hungeriness
-        self.filth = filth # stat for cleansliness
-        self.fun = fun # stat for not boredem-ness
+    food = 10
+    filth = 10
+    fun = 10
 
-
-        # defaults for survival stats, or the 'f' stats
-        # these stats should always start at max
-        # decrease over time
-        # something dying related when ANDOR these 0 is true
-        food = 10
-        filth = 10
-        fun = 10
-
-
-        # defaults for tracked stats
-        # always start at 0
-        # change over time, should only increase
-        age = 0
-        exp = 0
+    age = 0
+    exp = 0
