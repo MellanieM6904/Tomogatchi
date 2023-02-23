c7e15f14001bb9d142df5208b4115e97e31a9c61
Fiberoptic585 <sgtspring2020@gmail.com>
2023-02-23 14:41:34 -0800

    Update Tamagatchi.py

--- Tamagatchi.py
+++ Tamagatchi.py
@@ -3,10 +3,11 @@ class Pet:
     # construct class with these variables
     def __init__(self, name):
         self.name = name # name, used for filename of stored Pet
-        
+
+    # survival stats, 'f' stats
     food = 10
     filth = 10
     fun = 10
-
+    # tracked stats
     age = 0
     exp = 0
